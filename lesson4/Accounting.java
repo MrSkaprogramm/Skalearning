8ff015819b333ceb11c06ec7b6f84c2e74caf125
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-11-18 13:33:17 +0300

    Update Accounting.java

--- lesson4/Accounting.java
+++ lesson4/Accounting.java
@@ -5,6 +5,8 @@ public class Accounting {
 		Scanner sc = new Scanner(System.in);
 		int taxRate = Integer.MIN_VALUE;
 		int roadTax = Integer.MIN_VALUE;
+		int firmLegalEntityRevenue = Integer.MIN_VALUE;
+		int firmJSCRevenue = Integer.MIN_VALUE;
 		
 		while(taxRate < 0 || taxRate > 100) {
 			System.out.println("Enter taxRate value:");
@@ -14,13 +16,21 @@ public class Accounting {
 			System.out.println("Enter roadTax value:");
 			roadTax = sc.nextInt();
 		}
+		while(firmLegalEntityRevenue < 0) {
+			System.out.println("Enter LegalEntity revenue:");
+			firmLegalEntityRevenue = sc.nextInt();
+		}
+		while(firmJSCRevenue < 0) {
+			System.out.println("Enter JSC revenue:");
+			firmJSCRevenue = sc.nextInt();
+		}
 		sc.close();
 		
 		Accounting accounting = new Accounting();
 		LegalEntity legalentity = new LegalEntity(taxRate);
 		JSC jsc = new  JSC(taxRate, roadTax);
-		legalentity.setRevenue(5000000);
-		jsc.setRevenue(1000000);
+		legalentity.setRevenue(firmLegalEntityRevenue);
+		jsc.setRevenue(firmJSCRevenue);
 		accounting.firmLegalEntityBalance(legalentity);
 		accounting.firmJSCBalance(jsc);
 	}
