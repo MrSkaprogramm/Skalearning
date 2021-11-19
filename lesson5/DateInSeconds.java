28573c8f41e5ed5f9909aa336deb4518a9a24055
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-11-19 20:14:10 +0300

    Update DateInSeconds.java

--- lesson5/DateInSeconds.java
+++ lesson5/DateInSeconds.java
@@ -5,25 +5,20 @@ public class DateInSeconds {
 	public static void main(String[] args) {
 		DateInSeconds date = new DateInSeconds();
 		Scanner sc = new Scanner(System.in);
-		int weeks = Integer.MIN_VALUE;
-		int days = Integer.MIN_VALUE;
-		int hours = Integer.MIN_VALUE;
-		int minutes = Integer.MIN_VALUE;
-		int seconds = Integer.MIN_VALUE;
 		System.out.println("Enter the amount of time in seconds:");
 		int time = sc.nextInt();
 		sc.close();
 		
-		System.out.println ("Fully date converted from seconds is: " + date.dateInSeconds(time, weeks, days, hours, minutes, seconds));
+		System.out.println ("Fully date converted from seconds is: " + date.dateInSeconds(time));
 
 	}
 
-	public String dateInSeconds(int time, int weeks, int days, int hours, int minutes, int seconds) {
-		weeks = time / (7*24*60*60);
-		days = (time % (weeks * (7*24*60*60))) / (24*60*60);
-		hours = (time % (weeks * (7*24*60*60) + days * (24*60*60))) / (60*60);
-		minutes = (time % (weeks * (7*24*60*60) + days * (24*60*60) + hours * (60*60))) / 60;
-		seconds = (time % (weeks * (7*24*60*60) + days * (24*60*60) + hours * (60*60) + minutes * 60)) % 60;
+	public String dateInSeconds(int time) {
+		int weeks = time / (7*24*60*60);
+		int days = (time % (weeks * (7*24*60*60))) / (24*60*60);
+		int hours = (time % (weeks * (7*24*60*60) + days * (24*60*60))) / (60*60);
+		int minutes = (time % (weeks * (7*24*60*60) + days * (24*60*60) + hours * (60*60))) / 60;
+		int seconds = (time % (weeks * (7*24*60*60) + days * (24*60*60) + hours * (60*60) + minutes * 60)) % 60;
 		return weeks + " weeks " + days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds ";
 	}
 
