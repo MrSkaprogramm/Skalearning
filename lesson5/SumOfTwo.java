8dc2d7e43a73383a891f725ef659360fd2209d48
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-11-19 11:37:42 +0300

    Update SumOfTwo.java

--- lesson5/SumOfTwo.java
+++ lesson5/SumOfTwo.java
@@ -1,1 +1,20 @@
- 
+import java.util.Scanner;
+
+public class SumOfTwo {
+
+	public static void main(String[] args) {
+		SumOfTwo sum = new SumOfTwo();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number:");
+		int digit1 = sc.nextInt();
+		System.out.println("Enter the second number:");
+		int digit2 = sc.nextInt();
+		sc.close();
+		
+		System.out.println("Result: " + sum.sumOfTwo(digit1, digit2));
+	}
+
+	public int sumOfTwo(int digit1, int digit2) {
+		return (digit1 + digit2) + (digit1 * digit2);
+	}
+}
