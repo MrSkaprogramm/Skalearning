bcbe38c5677f8d62c53a02d9118d7f20bf97ff3a
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-11-22 23:54:34 +0300

    Update SumDigits.java

--- lesson5/SumDigits.java
+++ lesson5/SumDigits.java
@@ -1,19 +1,13 @@
 public class SumDigits {
 
 	public static void main(String[] args) {
-		long totalDigits = 7893823445L;
 		long digit = 7893823445L;
-		int count = 0;
 		long sumDigits = 0;
 		
-		while(totalDigits != 0) {
-			totalDigits /= 10;
-			count ++;
-		}
 		 do {
-			--count;
-			sumDigits += (digit / (long)Math.pow(10, count)) % 10;
-		} while(count > 0);
+			 sumDigits += (digit % 10);
+			 digit /= 10;
+		} while(digit != 0);
 		
 		System.out.println("Sum of digits in number 7893823445 equals: " + sumDigits);
 	}
