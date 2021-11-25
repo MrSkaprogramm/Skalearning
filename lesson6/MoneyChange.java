0934c0b46447f7fbff646c1abc8403bd0ed782de
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-11-25 12:04:03 +0300

    Update MoneyChange.java

--- lesson6/MoneyChange.java
+++ lesson6/MoneyChange.java
@@ -11,21 +11,25 @@ public class MoneyChange {
 		System.out.println("Your amount of money is: ");
 		switch(money % 10) {
 			case 1:
-				if((money / 100) > 0) {
+				if((money % 100) > 0) {
 					if(money % 100 > 10 && money % 100 < 20) {
 						System.out.println(money + " рублей");
 					}else {
 						System.out.println(money + " рубль");
 					}
+				}else {
+					System.out.println(money + " рубля");
 				}
 				break;
 			case 2, 3, 4:
-				if((money / 100) > 0) {
+				if((money % 100) > 0) {
 					if(money % 100 > 10 && money % 100 < 20) {
 						System.out.println(money + " рублей");
 					}else {
 						System.out.println(money + " рубля");
 					}
+				}else {
+					System.out.println(money + " рубля");
 				}
 				break;
 			default:
