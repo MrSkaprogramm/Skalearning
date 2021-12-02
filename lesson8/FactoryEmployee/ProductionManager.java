76758e7fb418f08af21822050680867f3159137d
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-03 01:55:48 +0300

    Create ProductionManager.java

---
+++ lesson8/FactoryEmployee/ProductionManager.java
@@ -0,0 +1,6 @@
+public class ProductionManager extends Managers {
+
+	public ProductionManager(int hours, int companyRevenue, int bonus, int percentRevenueRate) {
+		super(hours, companyRevenue, bonus, percentRevenueRate);
+	}
+}
