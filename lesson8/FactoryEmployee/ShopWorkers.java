3fb730503638c81842d975106e56b916cf9c38d0
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-03 02:01:23 +0300

    Create ShopWorkers.java

---
+++ lesson8/FactoryEmployee/ShopWorkers.java
@@ -0,0 +1,26 @@
+public class ShopWorkers extends Worksers {
+	private int hourlyRate;
+	private int hours;
+	
+	public ShopWorkers(int bonus, int hours) {
+		super(bonus);
+		this.hours = hours;
+	}
+	
+	public void setHourlyRate(int hourlyRate) {
+		this.hourlyRate = hourlyRate;
+	}
+	
+	public int getHourlyRate() {
+		return hourlyRate;
+	}
+	
+	public int getHours() {
+		return hours;
+	}
+	
+	public int calculateSalary() {
+		int Salary = hours * hourlyRate + ((hours*hourlyRate)/100)*getBonus();
+		return Salary;
+	}
+}
