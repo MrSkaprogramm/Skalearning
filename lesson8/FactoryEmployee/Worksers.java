59e6f8202e9d8d5e720014c8c6474feac8123c85
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-03 01:56:17 +0300

    Create Worksers.java

---
+++ lesson8/FactoryEmployee/Worksers.java
@@ -0,0 +1,11 @@
+public class Worksers extends FactoryEmployee {
+	private int bonus;
+	
+	public Worksers(int bonus) {
+		this.bonus = bonus;
+	}
+	
+	public int getBonus() {
+		return bonus;
+	}
+}
