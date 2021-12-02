c52644cc2003a2f3c5847b35deab9aba07c22462
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-03 01:55:08 +0300

    Create HeadOfProductSales.java

---
+++ lesson8/FactoryEmployee/HeadOfProductSales.java
@@ -0,0 +1,6 @@
+public class HeadOfProductSales extends Managers {
+	
+	public HeadOfProductSales(int hours, int companyRevenue, int bonus, int percentRevenueRate) {
+		super(hours, companyRevenue, bonus, percentRevenueRate);
+	}
+}
