0e61117317d1d65f15f89e4f0596666be29d8594
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-20 19:06:17 +0300

    Update OneMillionElements.java

--- lesson13/OneMillionElements.java
+++ lesson13/OneMillionElements.java
@@ -3,6 +3,36 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class OneMillionElements {
+	private double time;
+	private double arrayTime;
+	private double linkTime;
+	private static final int AMOUNT_ELEMENTS = 1000000;
+	private static final int CHOOSE_COUNT = 100000;
+	private static final double MILLIS_TO_SEC  = 1000.0;
+	
+	public void setTime(double time) {
+		this.time = time;
+	}
+	
+	public void setArrayTime(double arrayTime) {
+		this.arrayTime = arrayTime;
+	}
+	
+	public void setLinkTime(double linkTime) {
+		this.linkTime = linkTime;
+	}
+	
+	public double getTime() {
+		return time;
+	}
+	
+	public double getArrayTime() {
+		return arrayTime;
+	}
+	
+	public double getLinkTime() {
+		return linkTime;
+	}
 
 	public static void main(String[] args) {
 		OneMillionElements random = new OneMillionElements();
@@ -12,39 +42,72 @@ public class OneMillionElements {
 	public List<Integer> addMillionArray() {
 		List<Integer> millionArray = new ArrayList<>();
 		
-		for(int i = 0; i < 1000000; i++) {
+		setTime(System.currentTimeMillis()/MILLIS_TO_SEC);
+		for(int i = 0; i < AMOUNT_ELEMENTS; i++) {
 			millionArray.add((int)(Math.random()*10 + 1));
 		}
+		setArrayTime(arrayTime = System.currentTimeMillis()/MILLIS_TO_SEC - time);
+		System.out.println("To add items to ArrayList estimated time is: " + getArrayTime() + " seconds");
+		addInMiddleMillionArray(millionArray);
 		
 		return millionArray;
+	}
+	
+	public void addInMiddleMillionArray(List<Integer> millionArray) {
+		millionArray.clear();
+		
+		setTime(System.currentTimeMillis()/MILLIS_TO_SEC);
+		for(int i = 0; i < AMOUNT_ELEMENTS; i++) {
+			millionArray.add((millionArray.size()/2), (int)(Math.random()*10 + 1));;
+		}
+		setArrayTime(arrayTime = System.currentTimeMillis()/MILLIS_TO_SEC - time);
+		System.out.println("To add items to middle of ArrayList estimated time is: " + getArrayTime() + " seconds");
 	}
 	
 	public List<Integer> addMillionLink() {
 		List<Integer> millionLink = new LinkedList<>();
 		
-		for(int i = 0; i < 1000000; i++) {
+		setTime(System.currentTimeMillis()/MILLIS_TO_SEC);
+		for(int i = 0; i < AMOUNT_ELEMENTS; i++) {
 			millionLink.add((int)(Math.random()*10 + 1));
 		}
+		
+		setLinkTime(System.currentTimeMillis()/MILLIS_TO_SEC - time);
+		System.out.println("To add items to LinkedList estimated time is: " + getLinkTime() + " seconds");
+		addInMiddleMillionLink(millionLink);
+		
 		return millionLink;
 	}
 	
+	public void addInMiddleMillionLink(List<Integer> millionLink) {
+		millionLink.clear();
+		
+		setTime(System.currentTimeMillis()/MILLIS_TO_SEC);
+		for(int i = 0; i < AMOUNT_ELEMENTS; i++) {
+			if(i >= 2) {
+				millionLink.add((millionLink.size()/2), (int)(Math.random()*10 + 1));
+			}else {
+				millionLink.add((int)(Math.random()*10 + 1));
+			}
+		}
+		setLinkTime(System.currentTimeMillis()/MILLIS_TO_SEC - time);
+		System.out.println("To add items to middle of LinkedList estimated time is: " + getLinkTime() + " seconds");
+	}
+	
 	public void getRandom(List<Integer> millionArray, List<Integer> millionLink) {
-		long time;
-		long arrayTime;
-		long linkTime;
 		
-		time = System.currentTimeMillis();
-		for(int i = 0; i < 100000; i++) {
-			millionArray.get(((int)(Math.random()*10 + 1)) * 10000 - 1);
+		setTime(System.currentTimeMillis()/MILLIS_TO_SEC);
+		for(int i = 0; i < CHOOSE_COUNT; i++) {
+			millionArray.get(((int)(Math.random()*CHOOSE_COUNT)));
 		}
-		arrayTime = System.currentTimeMillis() - time;
-		System.out.println("To choose an item from ArrayList estimated time is: " + arrayTime + " milliseconds");
+		setArrayTime(System.currentTimeMillis()/MILLIS_TO_SEC - time);
+		System.out.println("To choose items from ArrayList estimated time is: " + getArrayTime() + " seconds");
 		
-		time = System.currentTimeMillis();
-		for(int i = 0; i < 100000; i++) {
-			millionLink.get(((int)(Math.random()*10 + 1)) * 10000 - 1);
+		setTime(System.currentTimeMillis()/MILLIS_TO_SEC);
+		for(int i = 0; i < CHOOSE_COUNT; i++) {
+			millionLink.get(((int)(Math.random()*CHOOSE_COUNT)));
 		}
-		linkTime = System.currentTimeMillis() - time;
-		System.out.println("To choose an item from LinkedList estimated time is: " + linkTime + " milliseconds");
+		setLinkTime(System.currentTimeMillis()/MILLIS_TO_SEC - time);
+		System.out.println("To choose items from LinkedList estimated time is: " + getLinkTime() + " seconds");
 	}
 }
