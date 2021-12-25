e6745c08192f784f39b172098ef24b510d208370
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-26 00:57:51 +0300

    Create HeavyBox.java

---
+++ lesson14/Boxes/HeavyBox.java
@@ -0,0 +1,25 @@
+public class HeavyBox {
+	private int size;
+	private int weight;
+	
+	public HeavyBox(int size, int weight) {
+		this.size = size;
+		this.weight = weight;
+	}
+	
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	public int getSize() {
+		return size;
+	}
+	
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+}
