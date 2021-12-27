480f627817398b27d08dbd93689594cb80370d54
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-27 21:27:58 +0300

    Update Pet.java

--- lesson14/Pets/Pet.java
+++ lesson14/Pets/Pet.java
@@ -1,3 +1,15 @@
 public class Pet {
+	private String petName;
+	
+	public Pet(String petName) {
+		this.petName = petName;
+	}
+	
+	public String getPetName() {
+		return petName;
+	}
 
+	public void setPetName(String petName) {
+		this.petName = petName;
+	}
 }
