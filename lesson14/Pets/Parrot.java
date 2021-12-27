9d747a606be12cb5eb1f4464d1ecf8324e0c16ff
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-27 21:28:21 +0300

    Update Parrot.java

--- lesson14/Pets/Parrot.java
+++ lesson14/Pets/Parrot.java
@@ -1,15 +1,5 @@
 public class Parrot extends Pet {
-	private String parrotName;
-	
-	public Parrot(String parrotName) {
-		this.parrotName = parrotName;
-	}
-	
-	public String getParrotName() {
-		return parrotName;
-	}
-
-	public void setParrotName(String parrotName) {
-		this.parrotName = parrotName;
+	public Parrot(String petName) {
+		super(petName);
 	}
 }
