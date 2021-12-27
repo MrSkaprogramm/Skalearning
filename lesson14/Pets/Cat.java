4d3991c1591218f51369d34db79ce20a5749b820
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-27 21:29:03 +0300

    Update Cat.java

--- lesson14/Pets/Cat.java
+++ lesson14/Pets/Cat.java
@@ -1,15 +1,5 @@
 public class Cat extends Pet {
-	private String catName;
-	
-	public Cat(String catName) {
-		this.catName = catName;
-	}
-
-	public String getCatName() {
-		return catName;
-	}
-
-	public void setCatName(String catName) {
-		this.catName = catName;
+	public Cat(String petName) {
+		super(petName);
 	}
 }
