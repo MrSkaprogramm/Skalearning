0480aa0edfb6f38c8929b3b0f54ab0feff73b1c3
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-27 20:01:45 +0300

    Create Parrot.java

---
+++ lesson14/Pets/Parrot.java
@@ -0,0 +1,15 @@
+public class Parrot extends Pet {
+	private String parrotName;
+	
+	public Parrot(String parrotName) {
+		this.parrotName = parrotName;
+	}
+	
+	public String getParrotName() {
+		return parrotName;
+	}
+
+	public void setParrotName(String parrotName) {
+		this.parrotName = parrotName;
+	}
+}
