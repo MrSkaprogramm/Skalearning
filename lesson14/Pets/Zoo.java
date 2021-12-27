8994d9e74d8d4ade0fe0717766f536fb87782b4d
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-27 21:26:10 +0300

    Update Zoo.java

--- lesson14/Pets/Zoo.java
+++ lesson14/Pets/Zoo.java
@@ -4,44 +4,20 @@ import java.util.Map;
 public class Zoo {
 
 	public static void main(String[] args) {
-		Zoo homeAnimals = new Zoo();
+		PetServiceImpl homeAnimals = new PetServiceImpl();
 		Map<String, Pet> zoo = new HashMap<>();
 		
-		Cat cat1 = new Cat("Garfield");
-		Cat cat2 = new Cat("Barsik");
-		Cat cat3 = new Cat("Fred");
-		Dog dog1 = new Dog("Bobby");
-		Dog dog2 = new Dog("Xavier");
-		Dog dog3 = new Dog("Tide");
-		Parrot parrot1 = new Parrot("Kuku");
-		Parrot parrot2 = new Parrot("Krack");
-		Parrot parrot3 = new Parrot("Oliver");
-		
-		zoo.put(cat1.getCatName(), cat1);
-		zoo.put(cat2.getCatName(), cat2);
-		zoo.put(cat3.getCatName(), cat3);
-		zoo.put(dog1.getDogName(), dog1);
-		zoo.put(dog2.getDogName(), dog2);
-		zoo.put(dog3.getDogName(), dog3);
-		zoo.put(parrot1.getParrotName(), parrot1);
-		zoo.put(parrot2.getParrotName(), parrot2);
-		zoo.put(parrot3.getParrotName(), parrot3);
-		
+		homeAnimals.addCat(zoo, "Garfield", homeAnimals.newCat("Garfield"));
+		homeAnimals.addCat(zoo, "Barsik", homeAnimals.newCat("Barsik"));
+		homeAnimals.addCat(zoo, "Fred", homeAnimals.newCat("Fred"));
+		homeAnimals.addDog(zoo, "Bobby", homeAnimals.newDog("Bobby"));
+		homeAnimals.addDog(zoo, "Xavier", homeAnimals.newDog("Xavier"));
+		homeAnimals.addDog(zoo, "Tide", homeAnimals.newDog("Tide"));
+		homeAnimals.addParrot(zoo, "Kuku", homeAnimals.newParrot("Kuku"));
+		homeAnimals.addParrot(zoo, "Krack", homeAnimals.newParrot("Krack"));
+		homeAnimals.addParrot(zoo, "Oliver", homeAnimals.newParrot("Oliver"));
+
 		homeAnimals.printZoo(zoo);
 		homeAnimals.printZooKeys(zoo);
-		
 	}
-	
-	public void printZoo(Map<String, Pet> zoo) {
-		for(Map.Entry<String, Pet> currentPet : zoo.entrySet()) {
-			System.out.println("Pet name: " + currentPet.getKey() + " | Type of pet: " + currentPet.getValue().getClass());
-		}
-	}
-	
-	public void printZooKeys(Map<String, Pet> zoo) {
-		for(String currentPet : zoo.keySet()) {
-			System.out.println("Pet name: " + currentPet);
-		}
-	}
-
 }
