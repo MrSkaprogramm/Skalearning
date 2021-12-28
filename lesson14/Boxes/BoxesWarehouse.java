85933ec0c90ba9b0b60b233a2ada427cffcb8dbf
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2021-12-28 03:24:28 +0300

    Update BoxesWarehouse.java

--- lesson14/Boxes/BoxesWarehouse.java
+++ lesson14/Boxes/BoxesWarehouse.java
@@ -9,10 +9,10 @@ public class BoxesWarehouse {
 		Set<HeavyBox> heavySet = new HashSet<>();
 		
 		HeavyBox heavyBox1 = worker.newHeavyBox(17, 130);
-		HeavyBox heavyBox2 = worker.newHeavyBox(17, 130);
-		HeavyBox heavyBox3 = worker.newHeavyBox(17, 130);
-		HeavyBox heavyBox4 = worker.newHeavyBox(17, 130);
-		HeavyBox heavyBox5 = worker.newHeavyBox(17, 130);
+		HeavyBox heavyBox2 = worker.newHeavyBox(14, 150);
+		HeavyBox heavyBox3 = worker.newHeavyBox(18, 170);
+		HeavyBox heavyBox4 = worker.newHeavyBox(20, 450);
+		HeavyBox heavyBox5 = worker.newHeavyBox(10, 145);
 		
 		worker.addBox(heavySet, heavyBox1);
 		worker.addBox(heavySet, heavyBox2);
@@ -22,7 +22,7 @@ public class BoxesWarehouse {
 		
 		worker.printBoxesSet(heavySet);
 		worker.changeBoxesWeight(heavySet);
-		worker.removeBoxes(heavySet, heavyBox4);
+		worker.removeBoxes(heavySet);
 		worker.heavySetToHeavyArr1(heavySet);
 		worker.heavySetToHeavyArr2(heavySet);
 		worker.heavySetToHeavyArr3(heavySet);
