eac01f7f72abfed304e9f9cfd54c522b69470cd4
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-01-05 12:48:58 +0300

    Create CatalogSortByPrice.java

---
+++ lesson16/E - ShopCatalog/CatalogSortByPrice.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+
+public class CatalogSortByPrice implements Comparator<Product> {
+
+	@Override
+	public int compare(Product o, Product o1) {
+		if(o.getProductPrice() != o1.getProductPrice()) {
+			return o.getProductPrice() - o1.getProductPrice();
+		}else if(o.getProductName() != o1.getProductName()){
+			return o.getProductName().compareTo(o1.getProductName());
+		}else if(o.getProductRating() != o1.getProductRating()){
+			return (int)(Math.ceil(o.getProductRating() - o1.getProductRating()));
+		}
+		return 0;
+	}
+}
