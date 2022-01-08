e5e8949fc4005195cb00dcb04c049ea4768b3c9c
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-01-08 17:40:45 +0300

    Create RandomNumbers.java

---
+++ lesson18/RandomNumbers.java
@@ -0,0 +1,55 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class RandomNumbers {
+	private static final int AMOUNT_OF_NUMBERS = 20;
+
+	public static void main(String[] args) {
+		RandomNumbers readBinary = new RandomNumbers();
+		String fileName = "out.bin";
+		readBinary.createBinFile(fileName);
+		readBinary.readBinFile(fileName);
+	}
+	
+	public void createBinFile(String fileName) {
+		try {
+			FileOutputStream fileOs = new FileOutputStream(fileName);
+			ObjectOutputStream objectOs = new ObjectOutputStream(fileOs);
+			int randomNum;
+			for(int i = 0; i < AMOUNT_OF_NUMBERS ; i++) {
+				randomNum = (int)(Math.random()*100);
+				objectOs.writeInt(randomNum);
+			}
+			objectOs.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Done writing. Now reading...");
+	}
+	
+	public void readBinFile(String fileName) {
+		try {
+			FileInputStream fileIn = new FileInputStream(fileName);
+			ObjectInputStream readFile = new ObjectInputStream(fileIn);
+			int x;
+			int sumNumbers = 0;
+			while(readFile.available() > 0) {
+				x = readFile.readInt();
+				System.out.print(x + " ");
+				sumNumbers += x;
+			}
+			System.out.print("\nArithmetic mean of numbers in file is: " + sumNumbers/AMOUNT_OF_NUMBERS);
+			readFile.close();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
