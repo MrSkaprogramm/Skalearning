c49629e89ddd98b53a58951504ef26a75e3123ad
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-01-18 12:49:09 +0300

    Update PersonServiceImpl.java

--- lesson18/Citizens/PersonServiceImpl.java
+++ lesson18/Citizens/PersonServiceImpl.java
@@ -19,15 +19,11 @@ public class PersonServiceImpl implements PersonService {
 		persons.add(person);
 	}
 	
-	public void createFilePersons(File file, List<Person> persons) {
-		try {
-			FileOutputStream addPerson = new FileOutputStream(file);
-			ObjectOutputStream addPersonObj = new ObjectOutputStream(addPerson);
+		public void createFilePersons(File file, List<Person> persons) {
+		try (FileOutputStream addPerson = new FileOutputStream(file); ObjectOutputStream addPersonObj = new ObjectOutputStream(addPerson);){
 			for(int i = 0; i < persons.size(); i++) {
 				addPersonObj.writeObject(persons.get(i));
 			}
-			addPerson.close();
-			addPersonObj.close();
 		}catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}catch (IOException e) {
@@ -37,15 +33,11 @@ public class PersonServiceImpl implements PersonService {
 
 	@Override
 	public void readPersonFile(File file, int amountPersons) {
-		try {
-			FileInputStream readPerson = new FileInputStream(file);
-			ObjectInputStream readPersonObj = new ObjectInputStream(readPerson);
+		try (FileInputStream readPerson = new FileInputStream(file); ObjectInputStream readPersonObj = new ObjectInputStream(readPerson);){
 			for(int i = 0; i < amountPersons; i++) {
 				Person person = (Person)readPersonObj.readObject();
 				System.out.printf("Person name: %s, Person surname: %s, Person age: %d\n", person.getName(), person.getSurname(), person.getAge());
 			}
-			readPerson.close();
-			readPersonObj.close();
 		}catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}catch (IOException e) {
