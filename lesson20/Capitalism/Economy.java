3618c45dbc1caa94092ae13b1d0f97b897f90969
MrSkaprogramm <91788166+MrSkaprogramm@users.noreply.github.com>
2022-01-22 23:18:54 +0300

    Update Economy.java

--- lesson20/Capitalism/Economy.java
+++ lesson20/Capitalism/Economy.java
@@ -8,15 +8,15 @@ public class Economy {
 	private static final int MAX_SATURATION_GOODS = 100;
 	private static final int FACTORY_STARTS_SATURATION_GOODS = 80;
 	private static final int TOTAL_ECONOMY_CAPACITY = 10000;
-	private static int economy_Capacity;
-	private static List<Integer> queue_Elements = new ArrayList<>();
+	private int economyCapacity;
+	private List<Integer> queue_Elements = new ArrayList<>();
 	
 	public int getEconomy_Capacity() {
-		return economy_Capacity;
+		return economyCapacity;
 	}
 
 	public void setEconomy_Capacity(int economy_Capacity) {
-		Economy.economy_Capacity = economy_Capacity;
+		this.economyCapacity = economy_Capacity;
 	}
 
 	public int getTotalEconomyCapacity() {
@@ -31,10 +31,7 @@ public class Economy {
 	}
 
 	public synchronized void producingCompany() {
-			System.out.println("produce1 " + Thread.currentThread().getName());
-				System.out.println("produce2 " + Thread.currentThread().getName());
 				while(queue_Elements.size() >= MAX_SATURATION_GOODS) {
-					System.out.println("produce3 " + Thread.currentThread().getName());
 					try {
 						System.out.println("wait produce");
 						wait();
@@ -43,30 +40,30 @@ public class Economy {
 					}
 					System.out.println(queue_Elements.size() + " in queue");
 				}
+				
 				notifyAll();
-					queue_Elements.add((int)(Math.random()*MAX_ELEMENTS_NUMBER));
-					System.out.println(economy_Capacity + " product produced | " + queue_Elements.size() + " in queue");
+				queue_Elements.add((int)(Math.random()*MAX_ELEMENTS_NUMBER));
+				System.out.println(economyCapacity + " product produced by " + Thread.currentThread().getName() + " factory");
 	}
-
+	
 	public synchronized void consumingCompany() {
-		if(economy_Capacity < TOTAL_ECONOMY_CAPACITY) {
-				System.out.println("consumer1 " + Thread.currentThread().getName());
-				while(queue_Elements.isEmpty()) {
-					try {
-						wait();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
+			while(queue_Elements.isEmpty()) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
+			}
 				
-				if(queue_Elements.size() <= FACTORY_STARTS_SATURATION_GOODS) {
-					notifyAll();
-				}
-				
-					queue_Elements.remove(0);
-					economy_Capacity++;
-					System.out.println(Thread.currentThread().getName());
-					System.out.println(economy_Capacity + " product consumed | " + queue_Elements.size() + " in queue");
+			if(queue_Elements.size() <= FACTORY_STARTS_SATURATION_GOODS) {
+				notifyAll();
+			}
+			
+			if(economyCapacity < TOTAL_ECONOMY_CAPACITY) {
+				queue_Elements.remove(0);
+				economyCapacity++;
+				System.out.println(Thread.currentThread().getName());
+				System.out.println(economyCapacity + " product consumed by " + Thread.currentThread().getName() + " shop");
 		}
 	}
 }
